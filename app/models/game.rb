79ea08383960003eef7cb2199f11c7080a6d72f6
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-12-23 21:04:08 -0800

    commit tree 631994777a0cf43670b025c92c01f83b7b2dd1db

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
   has_many :reviews
+  #order matters-you must place the first has_many that references the join table above the second has_many that goes through that join table. 
+  has_many :users, through: :reviews
+
 end
